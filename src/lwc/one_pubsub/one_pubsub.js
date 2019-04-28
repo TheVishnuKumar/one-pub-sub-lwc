38925ed5a3140393904aeb0604ae9e06f229fd8e
Vishnu Kumar <vishnu.kumar@mtxb2b.com>
2019-04-28 15:10:26 +0530

    Src commit

---
+++ src/lwc/one_pubsub/one_pubsub.js
@@ -0,0 +1,47 @@
+const callbacks = {};
+
+/**
+ * Registers a callback for an event
+ * @param {string} eventName - Name of the event to listen for.
+ * @param {function} callback - Function to invoke when said event is fired.
+ */
+const register = (eventName, callback) => {
+    if (!callbacks[eventName]) {
+        callbacks[eventName] = new Set();
+    }
+    callbacks[eventName].add(callback);
+};
+
+/**
+ * Unregisters a callback for an event
+ * @param {string} eventName - Name of the event to unregister from.
+ * @param {function} callback - Function to unregister.
+ */
+const unregister = (eventName, callback) => {
+    if (callbacks[eventName]) {
+        callbacks[eventName].delete(callback);
+    }
+};
+
+/**
+ * Fires an event to listeners.
+ * @param {string} eventName - Name of the event to fire.
+ * @param {*} payload - Payload of the event to fire.
+ */
+const fire = (eventName, payload) => {
+    if (callbacks[eventName]) {
+        callbacks[eventName].forEach(callback => {
+            try {
+                callback(payload);
+            } catch (error) {
+                // fail silently
+            }
+        });
+    }
+};
+
+export default {
+    register,
+    unregister,
+    fire,
+};
